a041ca5cf9575ea3e3cc5ba2446b2f2141dc37b6
anonymoussc <50c5ac69@opayq.com>
2015-09-08 06:12:09 +0700

    Its all about us

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,9 @@
 <?php
 
+// php artisan make:controller AboutController
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('about', 'AboutController', ['only' => ['index']]);
---
+++ resources/views/about/index.blade.php
@@ -0,0 +1,12 @@
+@extends('app')
+
+@section('content')
+
+    <h1>About Us</h1>
+
+    <p>All about us - Tatu</p>
+
+@endsection
+
+
+{{--Created by anonymoussc on 9/8/15 6:06 AM--}}
