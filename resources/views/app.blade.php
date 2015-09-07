d32b330cfb14777f63687b9803ec409a6f14be10
anonymoussc <50c5ac69@opayq.com>
2015-09-08 05:06:18 +0700

    Gulp elixir compile bootstrap-sass

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Welcome</title>
+    <link rel="stylesheet" href="/css/app.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </head>
 <body>
