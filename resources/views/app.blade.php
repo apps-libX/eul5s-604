b232d5bd39c27aed86a9532f01c5d7c1815b2853
anonymoussc <50c5ac69@opayq.com>
2015-09-07 21:49:39 +0700

    Preparing main layout, install lib dependencies

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+</head>
+<body>
+    <h1>Welcome Visitor</h1>
+    @yield('content')
+</body>
+</html>
+
+{{--Created by anonymoussc on 9/7/15 8:59 PM--}}
